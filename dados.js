c24342953898107bad8bf930489c4a3228f246a6
Matheus Yuri <msyf.business@gmail.com>
2024-09-06 20:28:47 -0300

    Função pesquisar alterada para aparecer conforme o que for pesquisado no site. chave e valor de tags para cada atleta.

--- dados.js
+++ dados.js
@@ -3,18 +3,21 @@ let dados = [
         titulo: "Rayssa Leal",
         descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
         link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
-        redesocial: "https://www.instagram.com/rayssalealsk8/"
+        redesocial: "https://www.instagram.com/rayssalealsk8/",
+        tags: "skate fada sk8"
     },
     {
         titulo: "Beatriz Souza",
         descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
         link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
-        redesocial: "https://www.instagram.com/_beatrizsouzaar/"
+        redesocial: "https://www.instagram.com/_beatrizsouzaar/",
+        tags: "judô judo judoca judoka ouro"
     },
     {
         titulo: "Rebeca Andrade",
         descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
         link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
-        redesocial: "https://www.instagram.com/rebecarandrade/"
+        redesocial: "https://www.instagram.com/rebecarandrade/",
+        tags: "ginástica ginastica ginasta ouro"
     }
 ];
