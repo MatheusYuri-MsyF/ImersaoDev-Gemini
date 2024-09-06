da942bbddf730f9bcec6c41b7958dedbc4206dc8
Matheus Yuri <msyf.business@gmail.com>
2024-09-05 22:51:21 -0300

    Redes sociais atualizadas no dados.js e função pesquisar implementada no html(com função básica de apenas ser chamada e aparecer todos os itens

--- dados.js
+++ dados.js
@@ -3,13 +3,13 @@ let dados = [
         titulo: "Rayssa Leal",
         descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
         link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
-        redesocial: "https://www.instagram.com/rayssalealoficial/"
+        redesocial: "https://www.instagram.com/rayssalealsk8/"
     },
     {
         titulo: "Beatriz Souza",
         descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
         link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
-        redesocial: "https://www.instagram.com/beatrizsouza_/"
+        redesocial: "https://www.instagram.com/_beatrizsouzaar/"
     },
     {
         titulo: "Rebeca Andrade",
